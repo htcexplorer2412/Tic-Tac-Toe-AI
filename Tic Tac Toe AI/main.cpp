f9587900e3ff155a0a3b2900f732d5eb85c6c9af
bansal patel <htcexplorer2412@gmail.com>
2024-08-11 23:43:46 +1000

    Added Simple Minimax Implementation

--- Tic Tac Toe AI/main.cpp
+++ Tic Tac Toe AI/main.cpp
@@ -1,5 +1,9 @@
 #include"iostream"
 
+struct Move {
+	int row, col;
+};
+
 void printBoard(char (& board)[3][3])
 {
 	for (auto i = 1; i < 4; i++)
@@ -17,6 +21,26 @@ void printBoard(char (& board)[3][3])
 	}
 }
 
+bool drawChecker(char(&board)[3][3])
+{
+	int num = 0;
+	for (auto i = 0; i < 3; i++)
+	{
+		for (auto j = 0; j < 3; j++)
+		{
+			if (board[i][j] == 'X' || board[i][j] == 'O')
+			{
+				num++;
+			}
+		}
+	}
+	if (num == 9)
+	{
+		return true;
+	}
+	return false;
+}
+
 void updateBoard(char (& board)[3][3], int index, char player)
 {
 	int i = (index - 1) / 3;
@@ -26,85 +50,204 @@ void updateBoard(char (& board)[3][3], int index, char player)
 
 }
 
-bool colRowChecker(char(&board)[3][3])
+int colRowChecker(char(&board)[3][3])
 {
 	for (auto i = 0; i < 3; i++)
 	{
 		if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
 		{
-			return true;
+			if (board[i][0] == 'X')
+			{
+				return 10;
+			}
+
+			else if (board[i][0] == 'O')
+			{
+				return -10;
+			}
+
 		}
 
 		else if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
 		{
-			return true;
-		}
-		
-		else
-		{
-			return false;
+			if (board[0][i] == 'X')
+			{
+				return 10;
+			}
+
+			else if (board[0][i] == 'O')
+			{
+				return -10;
+			}
+
 		}
 	}
+
+	return 0;
 }
 
-void gameStateChecker(char(&board)[3][3], bool &turn, bool& gameOver)
+int gameStateChecker(char(&board)[3][3])
 {
 	if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
 	{
-		gameOver = true;
-	}
-	else if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
-	{
-		gameOver = true;
-	}
-	else if (colRowChecker(board))
-	{
-		
-		gameOver = true;
+		if (board[0][0] == 'X')
+		{
+			return 10;
+		}
+
+		else if (board[0][0] == 'O')
+		{
+			return -10;
+		}
 	}
 
+	else if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+	{
+		if (board[0][2] == 'X')
+		{
+			return 10;
+		}
+
+		else if (board[0][2] == 'O')
+		{
+			return -10;
+		}
+	}
+
+	else 
+	{
+		return colRowChecker(board);
+	}
+
+	
+
+}
+
+int minimax(char (&board)[3][3], bool turn)
+{
+	int score = gameStateChecker(board);
+	std::cout << score << std::endl;
+
+	if (score == 10)
+	{
+		return score;
+	}
+		
+
+	if (score == -10)
+	{
+		return score;
+	}
+		
+
+	if (drawChecker)
+	{
+		return 0;
+	}
+
+	if (turn)
+	{
+		int best = -1000;
+
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				// Check if cell is empty 
+				if (board[i][j] == '_')
+				{
+					// Make the move 
+					board[i][j] = 'X';
+
+					// Call minimax recursively and choose 
+					// the maximum value 
+					best = std::max(best, minimax(board, !turn));
+
+					// Undo the move 
+					board[i][j] = '_';
+				}
+			}
+		}
+
+		return best;
+
+
+	}
+
+	else
+	{
+		int best = 1000;
+
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				// Check if cell is empty 
+				if (board[i][j] == '_')
+				{
+					// Make the move 
+					board[i][j] = 'O';
+
+					// Call minimax recursively and choose 
+					// the maximum value 
+					best = std::min(best, minimax(board, !turn));
+
+					// Undo the move 
+					board[i][j] = '_';
+				}
+			}
+		}
+		return best;
+
+	}
+		
+
+}
+
+Move findBestMove(char  (&board)[3][3])
+{
+	int bestVal = -1000;
+	Move bestMove;
+	bestMove.row = -1;
+	bestMove.col = -1;
+
+	for (auto i = 0; i < 3; i++)
+	{
+		for (auto j = 0; j < 3; j++)
+		{
+			if (board[i][j] == '_')
+			{
+				board[i][j] = 'X';
+
+				int moveVal = minimax(board, false);
+
+				board[i][j] = '_';
+
+				if (moveVal > bestVal)
+				{
+
+					bestMove.row = i;
+					bestMove.col = j;
+					bestVal = moveVal;
+				}
+			}
+		}
+	}
+	return bestMove;
 }
 
 int main()
 {
-	char board[3][3] = { {'1','2','3'} , {'4','5','6'} , {'7','8','9'} };
+	char board[3][3] = { {'X','O','O'} , {'O','X','X'} , {'_','_','_'} };
 	bool turn = true;
 	bool gameOver = false;
 	int  index = 0;
 	printBoard(board);
 
+	Move bestMove = findBestMove(board);
+	index = (3*bestMove.row) + bestMove.col;
 
-	while (!gameOver)
-	{
-		if (turn)
-		{
-			std::cout << "Player 1 Turn" << std::endl;
-			std::cout << "Please Select 1 to 9 to Display X"<< std::endl;
-			std::cin >> index;
-			updateBoard(board, index, 'X');
-			gameStateChecker(board, turn, gameOver);
-            turn = false;
-		}
-
-		else
-		{
-			std::cout << "Player 2 Turn" << std::endl;
-			std::cout << "Please select 1 to 9 to display O" << std::endl;
-			std::cin >> index;
-			updateBoard(board, index, 'O');
-			gameStateChecker(board, turn, gameOver);
-			turn = true;
-		}
-	}
-	if (!turn)
-	{
-		std::cout << "Game Over Player X won"<< std::endl;
-	}
-
-	else if (turn)
-	{
-		std::cout << "Game Over Player O won" << std::endl;
-	}
+	std::cout << "Best Index is " << index << " " << bestMove.row <<" " << bestMove.col << std::endl;
 	
     return 0;
 }
